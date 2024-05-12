5a2f57f7c1bf89b568be61127f7a0c086f12fbea
hyunjong <isc4614@naver.com>
2024-05-12 18:07:49 +0900

    gtk

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <errno.h>
@@ -24,10 +26,10 @@ static void activate(GtkApplication *app, gpointer user_data) {
     g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
     gtk_container_add(GTK_WINDOW(window), button);
 
-    gtk_widget_show(window);
+    gtk_widget_show_all(window);
 }
 
-int main(int argc, char **argv) {
+void run_gtk_application(int argc, char **argv) {
     GtkApplication *app;
     int status;
 
@@ -35,13 +37,11 @@ int main(int argc, char **argv) {
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
-
-    return status;
 }
 
-/*int main() {
+void run_command_line_interface() {
     int pid, fd;
-	SYMBOL term;
+    SYMBOL term;
 
     printf("[MiniOS SSU] Hello, World!\n");
     print_prompt();
@@ -50,22 +50,34 @@ int main(int argc, char **argv) {
 
     while(1) {
         printf("[SSU OS 8팀] (종료를 원하시면 exit를 입력해주세요.) :");
-        // readline을 사용하여 입력 받기
         term = parse(&pid, FALSE, NULL);
 
-        if (term != S_AMP && pid != 0)
-		{
-			waitfor(pid);
-		}
+        if (term != S_AMP && pid != 0) {
+            waitfor(pid);
+        }
 
-		if (term == S_NL)
-		{
-			print_prompt();
-		}
+        if (term == S_NL) {
+            print_prompt();
+        }
 
-		for (fd=3; fd<MAXFD; fd++)
-		{
-			close(fd);
-		}
+        for (fd = 3; fd < MAXFD; fd++) {
+            close(fd);
+        }
     }
-}*/
+}
+
+int main(int argc, char **argv) {
+    pid_t pid = fork();
+
+    if (pid == 0) { // Child process
+        run_gtk_application(argc, argv);
+    } else if (pid > 0) { // Parent process
+        run_command_line_interface();
+        wait(NULL); // Wait for the GTK application to finish
+    } else {
+        fprintf(stderr, "Failed to fork.\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
