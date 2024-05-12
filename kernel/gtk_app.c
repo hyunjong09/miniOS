e422a4ae3f95663a67b967000800beae5e5853f1
hyunjong <isc4614@naver.com>
2024-05-12 19:11:50 +0900

    11

--- kernel/gtk_app.c
+++ kernel/gtk_app.c
@@ -1,42 +1,8 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <errno.h>
+#include "gtk_app.h"
 
-void run_gtk_application(int argc, char **argv) {
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
-    exit(status);
-}
-
-static void on_button_clicked(GtkWidget *widget, gpointer data) {
-    FILE *fp;
-    char path[1035];
-
-    /* Open the command for reading. */
-    fp = popen("ls", "r");
-    if (fp == NULL) {
-        printf("Failed to run command\n");
-        exit(1);
-    }
-
-    /* Read the output a line at a time - output it. */
-    while (fgets(path, sizeof(path), fp) != NULL) {
-        printf("%s", path);
-    }
-
-    /* Close */
-    pclose(fp);
+static void print_hello(GtkWidget *widget, gpointer data) {
+    g_print("Hello, World!\n");
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
@@ -47,9 +13,19 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
-    button = gtk_button_new_with_label("Run ls");
-    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    button = gtk_button_new_with_label("Hello, World!");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
     gtk_container_add(GTK_CONTAINER(window), button);
 
     gtk_widget_show_all(window);
+}
+
+void run_gtk_application(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
 }
